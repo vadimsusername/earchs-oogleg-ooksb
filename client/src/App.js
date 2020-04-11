1ca5a56c74c1b2b3f833f4833f6009042982c155
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-11 00:28:29 -0400

    added boilerplate starting code

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+       <h1>Hello From App.js</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
